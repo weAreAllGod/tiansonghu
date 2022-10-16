0eb20b2719c5801da1b65da872cf3158b8e2909b
tiansonghu00 <115873583+tiansonghu00@users.noreply.github.com>
2022-10-16 17:33:39 +0800

    Add files via upload
    21意思是第二章第一个作业题，以此类推。

---
+++ homework23.java
@@ -0,0 +1,23 @@
+public class homework2.3 {
+    public static void main(String[] args){
+        String book1 = "十万个为什么1";
+        String book2 = "十万个为什么2";
+        System.out.println(book1 + book2);
+
+        char c1 = '男';
+        char c2 = '女';
+        System.out.println(c1 + c2);
+
+        float bprice1 = 151.24;
+        float bprice2 = 99.92;
+        System.out.println(bprice1 + bprice2);
+
+        String name = "zhangsan";
+        int age = 15;
+        double score = 80.5;
+        char sex = '女';
+        String hobby = "唱跳RAP篮球";
+        System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n"+ name + "\t" + age + + "\t" + score + "\t" + sex + "\t" + hobby);
+
+    }
+}
