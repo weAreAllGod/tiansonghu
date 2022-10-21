3c9a4c2cde27ba6a0b2bfcd7c40e8dbc34bb5a38
tiansonghu00 <115873583+tiansonghu00@users.noreply.github.com>
2022-10-21 15:47:53 +0800

    Update ifelse01.java

--- ifelse01.java
+++ ifelse01.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
         public static void main(String[] args) {
             //输入信用分，100信用极好，80-99，优秀；60-80一般；其他是不及格
             Scanner myScanner = new Scanner(System.in);
-            System.out.println("请输入信用分");
+            System.out.println("请输入信用分1-100");
             int score = myScanner.nextInt();
-            if (score == 100) {
-                System.out.println("信用极好");
-            } else if (score > 80 && score <= 99) {
-                System.out.println("信用优秀");
-            } else if (score >= 60 && score <= 80) {
-                System.out.println("信用一般");
+            if （score >= 1 && score <= 100){
+                if (score == 100) {
+                    System.out.println("信用极好");
+                } else if (score > 80 && score <= 99) {
+                    System.out.println("信用优秀");
+                } else if (score >= 60 && score <= 80) {
+                    System.out.println("信用一般");
+                } else {
+                    System.out.println("信用不及格");
+                }
             } else {
-                System.out.println("信用不及格");
-            }
+                System.out.println("输入不符合要求");
         }
     }
+}
