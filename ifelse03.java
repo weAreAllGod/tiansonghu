e85667d9da8859926b119017c74faee21ba4aba3
tiansonghu00 <115873583+tiansonghu00@users.noreply.github.com>
2022-10-21 16:38:51 +0800

    ifelse嵌套分支课后习题

---
+++ ifelse03.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ifelse03 {
+    public static void main(String[] args) {
+        //根据淡旺季的月份和年龄，打印票价
+        //4-10月旺季，大人18-60岁，60元；18岁以下，半价，老人三分之一价格
+        //淡季，大人40，其他人20
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("输入月份");
+        int month = myScanner.nextInt();
+        System.out.println("输入年龄");
+        int age = myScanner.nextInt();
+        if (month >=4 && month <= 10) {
+            if (age >=18 && age <=60) {
+                System.out.println("票价60");
+            } else if (age < 18) {
+                System.out.println("票价30");
+            } else if (age > 60) {
+                System.out.println("票价20");
+            }
+        } else  {
+            if (age >=18 && age <=60){
+                System.out.println("票价40");
+            } else {
+                System.out.println("票价20");
+            }
+        }
+    }
+}
