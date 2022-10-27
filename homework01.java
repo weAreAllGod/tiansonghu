4e7d87773d07337b5f1aa8bad3be8c2d12d8f326
tiansonghu00 <115873583+tiansonghu00@users.noreply.github.com>
2022-10-27 14:34:56 +0800

    Delete homework01.java

--- homework01.java
+++
@@ -1,6 +0,0 @@
-public class homework01 {
-    public static  void main(String[] args){
-        System.out.println("Hello worlfd");
-        System.out.println("姓名\t性别\t籍贯\t住址\n张三\t男\t山东\t家住山中");
-    }
-}
